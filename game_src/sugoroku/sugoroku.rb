23da4098610c60665b29c14b9ddb608dc60211b0
tequila <yuya.arukat@gmail.com>
2017-12-13 21:58:18 +0900

    プレイヤーリストクラス削除、ランキングクラス実装

--- game_src/sugoroku/sugoroku.rb
+++ game_src/sugoroku/sugoroku.rb
@@ -1,40 +1,14 @@
 require "io/console"
 
 class SugorokuPlayer
-  attr_accessor :rank, :goal
-  attr_reader :name, :outcome, :position
+  attr_accessor :position, :rank, :goal
+  attr_reader :name
 
   def initialize(name)
     @name = name
     @position = 0
     @rank = 0
     @goal = false
-    @dice = Dice.new(6)
-    @outcome = 0
-  end
-
-  def play
-    @outcome = @dice.roll
-    @position += @outcome
-  end
-end
-
-class PlayerList
-  attr_reader :players, :player_num
-
-  def initialize
-    @players = []
-    puts "何人でプレイしますか？"
-    @player_num = gets.chomp.to_i
-  end
-
-  def naming
-    ii = 1
-    while ii <= @player_num
-      puts "#{ii}人目の名前を入力してください。"
-      @players << SugorokuPlayer.new(gets.chomp)
-      ii += 1
-    end
   end
 end
 
@@ -51,39 +25,76 @@ class Dice
   end
 end
 
-class Sugoroku_main
-  def entry
-    @players = PlayerList.new
-    @players.naming
+class Ranking
+  def initialize
+    @ranking = []
+  end
+  def make_ranking(players)
+    @ranking = players.sort_by{|player| player.position}.reverse
+    @ranking.each_with_index{|player, idx| player.rank = idx + 1}
+  end
+  def display_ranking
+    ii = 0
+    while ii < @ranking.size
+      puts "現在#{@ranking[ii].rank}位は、#{@ranking[ii].name}さんです。"
+      ii += 1
+    end
+  end
+end
+
+class Sugoroku
+  attr_accessor :players
+
+  def initialize
+    @dice = Dice.new(6)
+    @rank = 1
+    @ranking = []
   end
 
-  def play
-    @players.players.each do |player|
-      puts "\n#{player.name}さんの番です。\nサイコロを振ってください！"
-      STDIN.getch
-      player.play
-      puts "\n#{player.outcome}が出ました！ #{player.name}さんは、現在#{player.position}マス目です"
+  def entry
+    @players = []
+    puts "何人でプレイしますか？"
+    player_num = gets.chomp.to_i
+
+    ii = 1
+    while ii <= player_num
+      puts "#{ii}人目の名前を入力してください。"
+      @players << SugorokuPlayer.new(gets.chomp)
+      ii += 1
     end
   end
 
-  def goal_judge?
-    @players.players.each do |player|
-      if (player.position >= 20)
+  def play
+    @players.each do |player|
+      unless player.goal
+        puts "\n#{player.name}さんの番です。\nサイコロを振ってください！"
+        STDIN.getch
+        @outcome = @dice.roll
+        player.position += @outcome
+        puts "\n#{@outcome}が出ました！ #{player.name}さんは、現在#{player.position}マス目です"
+      end
+    end
+  end
+
+  def finished?
+    @players.each do |player|
+      if (player.goal == false)&&(player.position >= 20)
         player.goal = true
       end
     end
   end
 
-  def make_ranking
-    it = @players.iterator
-    while it.hasNext?
-      player = it.next
-    end
+  def everybody_finished?
+    game_over = false
+    game_over = @players.inject(true){|result, player| result && player.goal}
   end
 end
 
-sugoroku = Sugoroku_main.new
+sugoroku = Sugoroku.new
+ranking = Ranking.new
 sugoroku.entry
-sugoroku.play
-sugoroku.play
-sugoroku.play
+until sugoroku.everybody_finished?
+  sugoroku.play
+  ranking.make_ranking(sugoroku.players)
+  ranking.display_ranking
+end
