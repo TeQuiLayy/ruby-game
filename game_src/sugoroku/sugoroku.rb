8d78bdf97069998c74f766e624d30186d5a75905
tequila <yuya.arukat@gmail.com>
2017-12-17 17:44:39 +0900

    変数名を修正

--- game_src/sugoroku/sugoroku.rb
+++ game_src/sugoroku/sugoroku.rb
@@ -36,15 +36,15 @@ class Sugoroku
     @players = []
     puts "何人でプレイしますか？"
     loop do
-      @player_num = gets.chomp.to_i
-      break if @player_num.to_s =~ /^[1-4]+$/
+      @num_players = gets.chomp.to_i
+      break if @num_players.to_s =~ /^[1-4]+$/
       puts "1〜4の半角数字を入力してください。"
     end
   end
 
   def entry
     ii = 1
-    while ii <= @player_num
+    while ii <= @num_players
       puts "#{ii}人目の名前を入力してください。"
       @players << SugorokuPlayer.new(gets.chomp)
       ii += 1
